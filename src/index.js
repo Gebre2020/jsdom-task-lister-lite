e2b0dffae1ebc90918718a38bae309181b64dd10
Gebremeskel <gebre20122@gmail.com>
2021-09-08 18:15:02 -0500

    compelet done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  // form and input values
+  const form = document.getElementById('create-task-form')
+  const ul = document.getElementById('tasks')
+
+  // Form submit event
+  form.addEventListener('submit', gettingInfo)
+
+  let num = 0;
+ 
+  function gettingInfo(e){
+    e.preventDefault()
+    const dropDown = document.getElementById('priority')
+    const dropDownValue = dropDown.options[dropDown.selectedIndex].value
+    console.log(dropDownValue)
+
+    let task = document.getElementById('new-task-description').value
+
+    ul.innerHTML += `<li id=${++num}>${task}<button class="btn">X</button></li>`
+    const deleteBtns = document.getElementsByClassName('btn')
+    const li = document.getElementById(`${num}`)
+
+    form.reset()
+    if(dropDownValue === "red"){
+       li.style.color = "red"
+       li.className = "1"
+   }else if(dropDownValue === "yellow"){
+     li.style.color = "yellow"
+     li.className = "2"
+   }else if(dropDownValue === "green"){
+     li.style.color = "green"
+     li.className = "3"
+   }
+
+    getToTask(deleteBtns)
+
+    let liNew = ul.children;
+    //debugger
+    [].slice
+    .call(liNew)
+    .sort(function(a,b){
+      return a.className - b.className;
+    })
+    .forEach(function(val){
+      ul.append(val);
+    })
+
+    // delete event
+    function getToTask(deleteBtns){
+      for (btn of deleteBtns){
+        btn.addEventListener("click", removeTask)
+      }
+     }
+     
+     function removeTask(e){
+       e.target.parentElement.remove()
+     }
+     
+    
+  }
+})
+
